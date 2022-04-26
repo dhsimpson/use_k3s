858a0ddd79645cbe69633c0d7cd0066e86b08769
dhsimpson <qeadzc9503@naver.com>
2022-04-27 03:07:12 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,39 +3,8 @@ curl -sfL https://get.k3s.io | sh -
 sudo chown ubuntu:ubuntu /etc/rancher/k3s/k3s.yaml
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-cat <<EOF > no-tls.yml 
-spec:
-  template:
-    spec:
-      containers:
-      - name: argocd-server
-        command: 
-          - argocd-server
-          - --insecure
-EOF
-kubectl patch deployment -n argocd argocd-server --patch-file no-tls.yml
+kubectl patch deployment -n argocd argocd-server --patch-file https://github.com/dhsimpson/kubeadm_on_ec2/blob/main/4.%20argocd%20%E1%84%8B%E1%85%B5%E1%84%8B%E1%85%AD%E1%86%BC%E1%84%92%E1%85%A2%20CD/no-tls.yml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.3/deploy/static/provider/baremetal/deploy.yaml
 kubectl delete validatingwebhookconfiguration ingress-nginx-admission
-cat <<EOF > argocd-ing.yml
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: argocd-server-ingress
-  annotations:
-    kubernetes.io/ingress.class: nginx
-    nginx.ingress.kubernetes.io/force-ssl-redirect: "true"
-    nginx.ingress.kubernetes.io/ssl-passthrough: "true"
-spec:
-  rules:
-    - http:
-        paths:
-          - pathType: Prefix
-            path: "/"
-            backend:
-              service:
-                name: argocd-server
-                port:
-                  number: 443
-EOF
-kubectl apply -n argocd -f argocd-ing.yml
+kubectl apply -n argocd -f https://github.com/dhsimpson/kubeadm_on_ec2/blob/main/4.%20argocd%20%E1%84%8B%E1%85%B5%E1%84%8B%E1%85%AD%E1%86%BC%E1%84%92%E1%85%A2%20CD/argocd-ing.yml
 
