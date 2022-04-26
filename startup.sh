f82165dfc894c34fc28a2f7b845df7a0a7bce966
dhsimpson <qeadzc9503@naver.com>
2022-04-27 02:59:29 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,7 @@ curl -sfL https://get.k3s.io | sh -
 sudo chown ubuntu:ubuntu /etc/rancher/k3s/k3s.yaml
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
 cat <<EOF > no-tls.yml 
 spec:
   template:
@@ -13,5 +14,6 @@ spec:
           - argocd-server
           - --insecure
 EOF
+
 kubectl patch deployment -n argocd argocd-server --patch-file no-tls.yml
 
